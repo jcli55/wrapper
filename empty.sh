360058fd63d8f9257cda0f254e3914cd83deec3b
Jean Li <jcli@wustl.edu>
2023-06-13 16:58:16 -0500

    added comments in the wrapper

--- empty.sh
+++ empty.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
+# Removes all files and directories in a given directory
+# Argument: need to pass in the target directory
+# Example: ./empty.sh [directory]
+#
+# Author: Jean.Li@bcm.edu
+# Date created: 6/13/2023
+
+# Pass in the target directory and save it as INDIR
 INDIR=$1
 
 function empty {
+	# Check to see if the INDIR is a directory
 	if [[ -d "$INDIR" ]]
 	then
+		# Check to see if the INDIR is not empty
 		if [[ "$(ls -A $INDIR)" ]]
 		then
+			# Remove the files/directories inside INDIR
 			rm -r "$INDIR"/*
 			echo "Removed all files from $INDIR"
 			return 1
